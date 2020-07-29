6f14549038afaf4012569a66d3035dea148de8d5
Sahar FRIKHA <sahar.frikha@etudiant-enit.utm.tn>
2020-07-29 11:26:49 +0100

    after meeting changes

---
+++ contact_me2.php
@@ -0,0 +1,49 @@
+<?php
+
+ 
+require 'smtp/PHPMailerAutoload.php';
+
+if (isset($_POST["submit"])) 
+{
+	$name = $_POST["name"];
+	$phone = $_POST["phone"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	
+
+	//Create a new PHPMailer instance
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
+	$mail->From = $email;
+	$mail->FromName = $name;
+	$mail->addAddress('info@uniqfst.com');
+	$mail->addCC('');
+	$mail->addReplyTo($email);
+	$mail->isHTML(true); 
+	$mail->Subject = 'Contact me';
+	$mail->Body    = '';
+	$mail->Body    .= '<p>Dear <strong>: UniQ Soft Technology Administrator</strong> </p>';
+	$mail->Body    .= '<p>Kindly be informed that you have recieved a contact demande you will find the needed info and message below :</p>';
+	$mail->Body    .= '<p>*		Name  : <strong>'.$name.'</strong> </p>';
+	$mail->Body    .= '<p>*		Phone  : <strong>'.$phone.'</strong> </p>';
+	$mail->Body    .= '<p>*		Email adresse : <strong>'.$email.'</strong> </p>';
+	$mail->Body    .= '<p>*		Demand : <strong>'.$message.'</strong> </p>';
+	$mail->Body    .= '<p>Best regards,</p>';
+	//
+
+	
+	if(!$mail->send()) {
+	    echo '<strong style="color: red;" >Message not sent! Try again another time</strong>';
+	    //echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		header('Location: index.html');
+		exit();
+	    //echo 'Message has been sent';
+	}
+
+	
+
+}
+header('Location: index.html');
+exit();
+?>
